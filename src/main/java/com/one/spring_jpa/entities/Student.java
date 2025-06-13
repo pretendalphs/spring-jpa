16398e738e78eadb8c66a06fb7e0f84d7a5698fd
pretend_alphs <abidkasu@gmail.com>
2025-06-13 19:16:30 +0530

    some changes

---
+++ src/main/java/com/one/spring_jpa/entities/Student.java
@@ -0,0 +1,73 @@
+package com.one.spring_jpa.entities;
+
+import java.util.Objects;
+
+public class Student {
+    private Integer id;
+    private Integer age;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public Student(Integer age,
+                   String firstName,
+                   String lastName,
+                   String email) {
+        this.age = age;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(id, student.id) && Objects.equals(age, student.age) && Objects.equals(firstName, student.firstName) && Objects.equals(lastName, student.lastName) && Objects.equals(email, student.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, age, firstName, lastName, email);
+    }
+}
